647d08ee5a52482a29c2d641397b62284e14746e
Iren <neridragonfire@gmail.com>
2022-10-22 11:41:59 +0300

    Шаблон бота 2

--- bot.py
+++ bot.py
@@ -7,12 +7,37 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from tg_bot.config import load_config
 
 
+# def register_all_middlewares(dp):
+#     dp.setup_middleware()
+
+
+# def register_all_filters(dp):
+#     dp.filters_factory.bind()
+
+
+def register_all_handlers(dp):
+
+
+
 async def main():
     config = load_config(".env")
     bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
-    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage
+    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
     dp = Dispatcher(bot, storage=storage)
+
     bot['config'] = config
+
+    # register_all_middlewares(dp)
+    # register_all_filters(dp)
+    register_all_handlers(dp)
+
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.session.close()
+
 
 if __name__ == "__main__":
     try:
