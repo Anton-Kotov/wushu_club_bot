8d9cfa7d2833332988c7eb7cbf2c295ae2701d27
Iren <neridragonfire@gmail.com>
2022-10-25 18:24:24 +0300

    Работа с БД1

--- tg_bot/handlers/start.py
+++ tg_bot/handlers/start.py
@@ -4,8 +4,8 @@ from aiogram.dispatcher.filters import CommandStart
 
 from tg_bot.keyboards.inline import register
 from tg_bot.keyboards.reply import first_time
-from tg_bot.models.psql_db import create_table, add_student_name, add_telegram_id, add_student_birthdate, \
-    add_student_style, add_student_level
+from tg_bot.models.psql_db import create_table, add_student_name, \
+    add_student_style, add_student_level, add_data, get_student
 
 
 async def bot_start(message: types.Message):
@@ -18,11 +18,10 @@ async def bot_start(message: types.Message):
     cursor = bot.get("cursor")
 
     # create_table(connection, cursor)
-    # add_student_name(connection, cursor)
-
-
+    print(get_student(cursor))
 
     await message.answer(text="Вы уже учитесь у нас или еще нет?", reply_markup=first_time)
+
 
 
 async def student(message: types.Message):
@@ -37,41 +36,53 @@ async def register_name(callback: types.CallbackQuery, state: FSMContext):
 
 
 async def register_birthdate(message: types.Message, state: FSMContext):
-    connection, cursor = message.bot["connection"], message.bot["cursor"]
-    telegram_id = message.from_user.id
-    name = message.text
-    add_telegram_id(connection, cursor, telegram_id)
-    add_student_name(connection, cursor, name)
+
+    async with state.proxy() as data:
+        data["name"] = message.text
+
     await message.answer(text="Дата рождения: дд.мм.гггг")
     await state.set_state("birthdate")
 
+
 async def register_start_data(message: types.Message, state: FSMContext):
-    connection, cursor = message.bot["connection"], message.bot["cursor"]
-    birthdate = message.text
-    add_student_birthdate(connection, cursor, birthdate)
+
+    async with state.proxy() as data:
+        data["birthdate"] = message.text
     await message.answer(text="Год, когда начали заниматься в центре УЦЗИМЭНЬ:")
     await state.set_state("start_data")
 
-async def register_start_date(message: types.Message, state: FSMContext):
-    connection, cursor = message.bot["connection"], message.bot["cursor"]
-    start_date = message.text
-    add_student_style(connection, cursor, start_date)
+
+async def register_style(message: types.Message, state: FSMContext):
+
+    async with state.proxy() as data:
+        data["start_date"] = message.text
+
     await message.answer(text="Какие стили ушу вы практикуете:")
     await state.set_state("style")
 
-async def register_style(message: types.Message, state: FSMContext):
-    connection, cursor = message.bot["connection"], message.bot["cursor"]
-    style = message.text
-    add_student_style(connection, cursor, style)
+async def register_level(message: types.Message, state: FSMContext):
+
+    async with state.proxy() as data:
+        data["style"] = message.text
+
     await message.answer(text="Какой у вас уровень?")
     await state.set_state("level")
 
 async def register_final(message: types.Message, state: FSMContext):
     connection, cursor = message.bot["connection"], message.bot["cursor"]
+    telegram_id = message.from_user.id
     level = message.text
+    data = await state.get_data()
+
+    add_student_name(connection, cursor, data["name"])
+    add_data(connection, cursor, telegram_id, data["birthdate"], data["start_date"])
+    add_student_style(connection, cursor, data["style"])
     add_student_level(connection, cursor, level)
-    await message.answer(text="Какой у вас уровень?")
+
+    await message.answer(text="Благодарим за регистрацию")
+    await state.reset_data()
     await state.finish()
+
 
 
 def register_start(dp: Dispatcher):
@@ -80,7 +91,8 @@ def register_start(dp: Dispatcher):
     dp.register_callback_query_handler(register_name, text="register")
     dp.register_message_handler(register_birthdate, state="name")
     dp.register_message_handler(register_start_data, state="birthdate")
-    dp.register_message_handler(register_style, state="style")
+    dp.register_message_handler(register_style, state="start_data")
+    dp.register_message_handler(register_level, state="style")
     dp.register_message_handler(register_final, state="level")
 
 
--- tg_bot/models/psql_db.py
+++ tg_bot/models/psql_db.py
@@ -7,26 +7,30 @@ def create_table(connection, cursor):
     cursor.execute(sql.read())
     connection.commit()
 
-def add_telegram_id(connection, cursor, telegram_id):
-    cursor.execute("INSERT INTO data (TELEGRAM_ID) VALUES (%s)", (f"{telegram_id}",))
-    connection.commit()
 
 def add_student_name(connection, cursor, name):
-    cursor.execute("INSERT INTO students_name (NAME) VALUES (%s)", (f"{name}",))
+    cursor.execute("INSERT INTO students_name (NAME) VALUES (%s)", (name,))
     connection.commit()
 
-def add_student_birthdate(connection, cursor, birthdate):
-    cursor.execute("INSERT INTO data (BIRTHDATE) VALUES (%s)", (f"{birthdate}",))
+
+def add_data(connection, cursor, telegram_id, birthdate, start_date):
+    cursor.execute("INSERT INTO data (TELEGRAM_ID, BIRTHDATE, START_DATE) VALUES (%s,%s,%s)",
+                   (telegram_id, birthdate, start_date))
     connection.commit()
 
-def add_start_date(connection, cursor, start_date):
-    cursor.execute("INSERT INTO data (START_DATE) VALUES (%s)", (f"{start_date}",))
-    connection.commit()
 
 def add_student_style(connection, cursor, style):
-    cursor.execute("INSERT INTO styles (STYLE) VALUES (%s)", (f"{style}",))
+    cursor.execute("INSERT INTO styles (STYLE) VALUES (%s)", (style,))
     connection.commit()
 
+
 def add_student_level(connection, cursor, level):
-    cursor.execute("INSERT INTO levels (LEVEL) VALUES (%s)", (f"{level}",))
+    cursor.execute("INSERT INTO levels (LEVEL) VALUES (%s)", (level,))
     connection.commit()
+
+
+def get_student(cursor):
+    cursor.execute("SELECT NAME "
+                   "FROM common_info "
+                   "INNER JOIN students_name ON common_info.student_id = students_name.student_id;")
+    return cursor.fetchall()
